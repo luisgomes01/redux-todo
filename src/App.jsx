1ce33434e09483857953e80da405e0a987c177bb
Luís Gomes <luis252001@hotmail.com>
2024-03-22 10:51:44 -0300

    feat: add plusicon to submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { PlusIcon } from '@heroicons/react/24/outline'
 
 const App = () => (
   <main className='flex items-center justify-center'>
-    <form className='w-3/4 flex flex-col'>
+    <form className='w-2/4 flex flex-col self-center'>
       <div className='flex relative'>
-        <input className='block m-0 p-3 rounded w-full' type='text' placeholder='Escreva aqui seu todo' />
-        <button className='absolute right-0 h-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 aspect-square border-blue-700 rounded' type='submit'>+</button>
+        <input className='block m-0 p-3 rounded w-full' type='text' placeholder='Todo' />
+        <button
+          className='absolute right-0 h-full bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 aspect-square border-blue-700 rounded'
+          type='submit'>
+          <PlusIcon />
+
+        </button>
       </div>
     </form>
   </main>
